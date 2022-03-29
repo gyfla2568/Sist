5481de57ac54dc2ab30213af79718f0327fb9fed
gyfla2568 <gyfla2568@gmail.com>
2022-03-29 16:35:02 +0900

    webwork

---
+++ HtmlProject/src/main/webapp/js/popup.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+ 
+ function pop1() {
+    window.open('../bg/gameimage/01.png','bg','width=500,height=500,scrollbars=yes');
+    }
+    
+  
+ function pop3() {
+    window.open('../bg/gameimage/02.png','Event','width=430,height=430');
+    }
+    
+newWin=window.open('../popup/popup4.jpg','Event','width=430,height=430');
+setTimeout('newWin.close()',3000);
+
+/*setTimeout(function(){},밀리초)*/
